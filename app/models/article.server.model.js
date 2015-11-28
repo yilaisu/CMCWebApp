9ddf40fc3cd0b740558c009dd5074aee1394eedf
mattlhy <mattlhy77@gmail.com>
2015-11-28 13:48:55 -0500

    test commit

---
+++ app/models/article.server.model.js
@@ -0,0 +1,65 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Article Schema
+ */
+var ArticleSchema = new Schema({
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	title: {
+		type: String,
+		default: '',
+		trim: true,
+		required: 'Title cannot be blank'
+	},
+	content: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	},
+	link: {
+		type: String,
+		required: 'Link cannot be blank'
+	},
+	price: {
+		type: Number,
+		default: '0'
+	},
+	totalTickets: {
+		type: Number,
+		required: 'Total tickets cannot be blank'
+	},
+	soldTickers: {
+		type: Number,
+		default: '0'
+	},
+	imageLink: {
+		type: String,
+		required: 'Image link should not be empty'
+	},
+	eventDate: {
+		type: Date,
+		required: 'Event date cannot be empty'
+	},
+	location: {
+		type: String,
+		required: 'Location cannot be empty'
+	},
+	fontColor: {
+		type: String
+	}
+});
+
+mongoose.model('Article', ArticleSchema);
